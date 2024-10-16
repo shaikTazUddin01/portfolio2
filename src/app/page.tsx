a4b157f34bc082d2d778666521cf834b9bdc9886
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-10-16 20:58:22 +0600

    added skill section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import About from "@/components/ui/About";
 import HeroArea from "@/components/ui/HeroArea";
+import MySkills from "@/components/ui/MySkills";
 
 export default function Home() {
   return (
 <div>
   {/* hero area */}
   <HeroArea/>
+  <div className="max-w-7xl mx-auto">
+
   <About/>
+  <MySkills/>
+  </div>
 </div>
   );
 }
--- src/components/ui/MySkills.tsx
+++ src/components/ui/MySkills.tsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import { Progress } from "@nextui-org/react";
+import React from "react";
 
 const MySkills = () => {
-    return (
-        <div>
-            <h1>My Skills</h1>
+  return (
+    <div className="py-10">
+      <h1 className="text-4xl text-center">My Skills</h1>
+      <div className="grid grid-cols-2 py-10 gap-2 w-full px-10 ">
+        <div className="w-[95%]"> 
+          <h1>HTML</h1>
+          <Progress size="lg" aria-label="html" value={95}  />
         </div>
-    );
+        <div className="w-[95%]">
+          <h1>CSS</h1>
+          <Progress size="lg" aria-label="css" value={90} />
+        </div>
+        <div className="w-[95%]">
+          <h1>Tailwind</h1>
+          <Progress size="lg" aria-label="tailwind" value={90} />
+        </div>
+        <div className="w-[95%]">
+          <h1>JAVASCRIPT</h1>
+          <Progress size="lg" aria-label="javascript" value={85} />
+        </div>
+        <div className="w-[95%]">
+          <h1>TYPESCRIPT</h1>
+          <Progress size="lg" aria-label="typescript" value={85} />
+        </div>
+        <div className="w-[95%]">
+          <h1>REACT</h1>
+          <Progress size="lg" aria-label="react" value={95} />
+        </div>
+        <div className="w-[95%]">
+          <h1>REDUX</h1>
+          <Progress size="lg" aria-label="redux" value={95} />
+        </div>
+        <div className="w-[95%]">
+          <h1>NEXT.js</h1>
+          <Progress size="lg" aria-label="next.js" value={80} />
+        </div>
+        <div className="w-[95%]">
+          <h1>NODE.js</h1>
+          <Progress size="lg" aria-label="node.js" value={90} />
+        </div>
+        <div className="w-[95%]">
+          <h1>EXPRESS.js</h1>
+          <Progress size="lg" aria-label="express.js" value={90} />
+        </div>
+        <div className="w-[95%]">
+          <h1>MONGODB</h1>
+          <Progress size="lg" aria-label="mongodb" value={90} />
+        </div>
+        <div className="w-[95%]">
+          <h1>MONGOOSE</h1>
+          <Progress size="lg" aria-label="mongoose" value={92} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MySkills;
